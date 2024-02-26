9db12aa8e386539297f4d44616a8dfce5802a9ba
hgm-king <hgmaxwellking@gmail.com>
2024-02-26 12:03:25 -0500

    Update consumer_group documentation

--- src/consumer_group.rs
+++ src/consumer_group.rs
@@ -20,18 +20,23 @@
 //!
 //! ### Example
 //! ```rust
-//! let consumer_group_member = consumer_group::ConsumerGroupBuilder::new(
-//!     bootstrap_url,
-//!     "Squad",
-//!     HashMap::from([(src_topic, vec![0, 1, 2, 3])]),
+//! let bootstrap_addrs = vec!["127.0.0.1:9092".to_string()];
+//! let partitions = vec![0];
+//! let topic_name = "my-topic";
+//! let assignment = std::collections::HashMap::from([(topic_name.to_string(), partitions)]);
+//! let group_id = "The Data Boyz".to_string();
+//! 
+//! let consumer_group_member = samsa::prelude::ConsumerGroupBuilder::new(
+//!     bootstrap_addrs,
+//!     group_id,
+//!     assignment,
 //! ).await?
-//! .build().await?
-//!
+//! .build().await?;
+//! 
 //! let stream = consumer_group_member.into_stream();
-//!
 //! // have to pin streams before iterating
 //! tokio::pin!(stream);
-//!
+//! 
 //! // Stream will do nothing unless consumed.
 //! while let Some(batch) = stream.next().await {
 //!     println!("{:?}", batch);
