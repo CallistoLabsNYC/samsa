e39e358ee2f07592b2331a821615344e9c11f375
David Rauschenbach <david@megalithic.llc>
2024-02-29 05:47:13 -0800

    Fix: Cannot perform ConsumerBuilder::build() via a reference to a consumer

--- tests/consumer_builder.rs
+++ tests/consumer_builder.rs
@@ -12,3 +12,15 @@ async fn it_can_build_with_minimal_args() -> Result<(), Box<Error>> {
     let _consumer = builder.build();
     Ok(())
 }
+
+#[tokio::test]
+async fn it_can_build_with_ref_to_builder() -> Result<(), Box<Error>> {
+    let (skip, brokers, _topic) = testsupport::get_brokers_and_topic()?;
+    if skip {
+        return Ok(());
+    }
+    let builder = ConsumerBuilder::new(brokers, TopicPartitions::default()).await?;
+    let builder_ref = &builder;
+    let _consumer = builder_ref.clone().build();
+    Ok(())
+}
