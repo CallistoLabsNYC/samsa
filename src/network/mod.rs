2a468058808e3511e6134aea71167a0b370dbfca
David Rauschenbach <david@megalithic.llc>
2024-04-23 08:33:09 -0700

    Introduce a BrokerConnection trait

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -34,7 +34,45 @@
 //! The server has a configurable maximum limit on request size and any
 //! request that exceeds this limit will result in the socket being
 //! disconnected.
-
 pub mod tcp;
 // #[cfg(feature = "tls")]
 pub mod tls;
+
+use crate::prelude::{encode::ToByte, Result};
+use bytes::BytesMut;
+
+pub trait BrokerConnection {
+    /// Serialize a given request and send to Kafka/Redpanda broker.
+    ///
+    /// The Kafka protocol specifies that all requests will
+    /// be processed in the order they are sent and responses will return in
+    /// that order as well. Users of this method should be sure to use the receive_response method
+    /// to accept Kafka responses in the order that these requests are sent.
+    ///
+    /// This method is only useful in practice when used in combination with
+    /// a request type. To see how this would be done, visit the protocol module.
+    ///
+    /// ### Example
+    /// ```
+    /// // send an arbitrary set of bytes to kafka broker
+    /// let buf = "test";
+    /// conn.send_request(buf).await?;
+    /// ```
+    async fn send_request<R: ToByte>(&self, req: &R) -> Result<()>;
+
+    /// Receive a response in raw bytes from a Kafka/Redpanda broker.
+    ///
+    /// Kafka queues up responses on the socket as requests are sent by the client.
+    /// This method pulls 1 request off the socket at a time and returns the raw bytes.
+    ///
+    /// This method returns raw data that is not useful until parsed
+    /// into a response type. To see how this would be done, visit the
+    /// protocol module.
+    ///
+    /// ### Example
+    /// ```
+    /// // receive a message from a kafka broker
+    /// let response_bytes = conn.receive_response().await?;
+    /// ```
+    async fn receive_response(&mut self) -> Result<BytesMut>;
+}
