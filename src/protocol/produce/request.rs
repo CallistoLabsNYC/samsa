25f1bfe28e9707bab464464a029c749c8d3b433c
hgm-king <hgmaxwellking@gmail.com>
2024-04-24 11:48:38 -0400

    clippy

--- src/protocol/produce/request.rs
+++ src/protocol/produce/request.rs
@@ -279,7 +279,7 @@ impl From<i16> for Attributes {
     fn from(n: i16) -> Self {
         // if the 1 bit is on, then its GZIP compression
         // technically ignoring other compression types for now
-        let compression = if n % 2 == 0 {
+        let compression = if n % 2 == 1 {
             Some(Compression::Gzip)
         } else {
             None
