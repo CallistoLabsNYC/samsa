adedace41600149149530d3e298e57b4a4ec4884
Oghenemarho <orukele.dev@gmail.com>
2024-07-16 14:38:12 +0200

    Add integration tests for Compression, large reads and writes, and multiple partitions #80 (#84)
    * updated readme and example to reflect the TLS and Compression feature
    
    * updated readme and lib.rs to reflect API change
    
    * first attempt of writing using compression setup
    
    * fixed some fmt issues
    
    * implemented multi partition test
    
    * fixed fmt issues
    
    * added batching for 1m messages
    
    * added test for 1 million read and write
    
    * finished all test
    
    * fixed fmt issues
    
    * updated 1M read and write test to use builder classes
    
    * used higher level constructors for producing and reading

---
+++ tests/multi_partitions_setup.rs
@@ -0,0 +1,101 @@
+use std::collections::HashMap;
+
+use futures::stream::iter;
+use futures::StreamExt;
+
+use samsa::prelude;
+use samsa::prelude::{
+    BrokerConnection, ConsumerBuilder, Error, KafkaCode, ProduceMessage, ProducerBuilder,
+    TcpConnection, TopicPartitionsBuilder,
+};
+
+mod testsupport;
+
+const CLIENT_ID: &str = "multi partition read and write test";
+const CORRELATION_ID: i32 = 1;
+const NUMBER_OF_PARTITIONS: i32 = 10;
+const CHUNK_SIZE: usize = 10;
+
+#[tokio::test]
+async fn multi_partition_writing_and_reading() -> Result<(), Box<Error>> {
+    let (skip, brokers) = testsupport::get_brokers()?;
+    if skip {
+        return Ok(());
+    }
+    let conn = TcpConnection::new(brokers.clone()).await?;
+    let topic_name = "tester-creation-partition";
+
+    //
+    // Create topic with 10 partitions
+    //
+    let create_res = prelude::create_topics(
+        conn.clone(),
+        CORRELATION_ID,
+        CLIENT_ID,
+        HashMap::from([(topic_name, NUMBER_OF_PARTITIONS)]),
+    )
+    .await?;
+
+    // TopicAlreadyExists is an acceptable error in the instance of this test
+    // aborting prematurely
+    if create_res.topics[0].error_code != KafkaCode::TopicAlreadyExists {
+        assert_eq!(create_res.topics[0].error_code, KafkaCode::None);
+    }
+
+    //
+    // Test producing (writing)
+    //
+    let stream = iter(0..NUMBER_OF_PARTITIONS).map(|i| ProduceMessage {
+        topic: topic_name.to_string(),
+        partition_id: i,
+        key: None,
+        value: Some(bytes::Bytes::from_static(b"0123456789")),
+        headers: vec![],
+    });
+
+    let output_stream =
+        ProducerBuilder::<TcpConnection>::new(brokers.clone(), vec![topic_name.to_string()])
+            .await?
+            .required_acks(1)
+            .clone()
+            .build_from_stream(stream.chunks(CHUNK_SIZE))
+            .await;
+    tokio::pin!(output_stream);
+    // producing
+    while let Some(message) = output_stream.next().await {
+        let res = message[0].as_ref().unwrap();
+        assert_eq!(res.responses.len(), 1);
+        assert_eq!(res.responses[0].name, bytes::Bytes::from(topic_name));
+        assert_eq!(
+            res.responses[0].partition_responses[0].error_code,
+            KafkaCode::None
+        );
+    }
+    // done
+
+    //
+    // Test fetch
+    //
+    let partitions: Vec<i32> = (0..NUMBER_OF_PARTITIONS).collect();
+    let stream = ConsumerBuilder::<TcpConnection>::new(
+        brokers.clone(),
+        TopicPartitionsBuilder::new()
+            .assign(topic_name.to_string(), partitions)
+            .build(),
+    )
+    .await?
+    .build()
+    .into_stream();
+
+    tokio::pin!(stream);
+    while let Some(message) = stream.next().await {
+        // assert topic name
+        let res = message.unwrap().0;
+        if !res.is_empty() {
+            assert_eq!(res[0].topic_name, bytes::Bytes::from(topic_name));
+            break;
+        }
+    }
+
+    Ok(())
+}
---
+++ tests/write_and_read_1m_messages.rs
@@ -0,0 +1,77 @@
+use futures::stream::iter;
+use futures::StreamExt;
+use samsa::prelude::{
+    BrokerConnection, Compression, ConsumerBuilder, Error, ProduceMessage, ProducerBuilder,
+    TcpConnection, TopicPartitionsBuilder,
+};
+
+mod testsupport;
+const CLIENT_ID: &str = "write and read 1M messages";
+const CORRELATION_ID: i32 = 1;
+const PARTITION_ID: i32 = 0;
+const CHUNK_SIZE: usize = 50_000;
+
+#[tokio::test]
+async fn write_and_read_1m_messages() -> Result<(), Box<Error>> {
+    let (skip, brokers) = testsupport::get_brokers()?;
+    if skip {
+        return Ok(());
+    }
+    let topic = "1m-test-topic";
+    let conn = TcpConnection::new(brokers.clone()).await?;
+    testsupport::ensure_topic_creation(conn, topic, CORRELATION_ID, CLIENT_ID).await?;
+
+    //
+    // Test writing
+    //
+    let stream = iter(0..1_000_000).map(|_| ProduceMessage {
+        topic: topic.to_string(),
+        partition_id: PARTITION_ID,
+        key: None,
+        value: Some(bytes::Bytes::from_static(b"0123456789")),
+        headers: vec![],
+    });
+
+    let output_stream =
+        ProducerBuilder::<TcpConnection>::new(brokers.clone(), vec![topic.to_string()])
+            .await?
+            .compression(Compression::Gzip)
+            .required_acks(1)
+            .clone()
+            .build_from_stream(stream.chunks(CHUNK_SIZE))
+            .await;
+    tokio::pin!(output_stream);
+    // producing
+    while let Some(message) = output_stream.next().await {
+        let res = message[0].as_ref().unwrap();
+        assert_eq!(res.responses.len(), 1);
+        assert_eq!(res.responses[0].name, bytes::Bytes::from(topic));
+    }
+    // done
+
+    //
+    // Test fetch (read)
+    //
+    let stream = ConsumerBuilder::<TcpConnection>::new(
+        brokers.clone(),
+        TopicPartitionsBuilder::new()
+            .assign(topic.to_string(), vec![0])
+            .build(),
+    )
+    .await?
+    .build()
+    .into_stream();
+
+    let mut counter = 0;
+    tokio::pin!(stream);
+    while let Some(_message) = stream.next().await {
+        counter += 1;
+        if counter == 1_000_000 {
+            break;
+        }
+    }
+
+    assert_eq!(counter, 1_000_000);
+
+    Ok(())
+}
