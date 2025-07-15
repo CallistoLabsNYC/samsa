4d1bb571b6676d5aede3e04dcac398134aa38298
alexey.nrdhm <alexey.nrdhm@gmail.com>
2025-07-15 21:31:13 +0300

    fix clippy

--- src/error.rs
+++ src/error.rs
@@ -37,7 +37,7 @@ pub enum Error {
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:?}", self)
+        write!(f, "{self:?}")
     }
 }
 
