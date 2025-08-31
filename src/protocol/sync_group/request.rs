3801c2f59e3dc6d72b2a4e43eeb15aaa6d30435e
Daniel Honig <daniel@boomer.digital>
2025-08-31 10:55:25 -0400

    Dth/fix ellision warning (#130)
    * fix ellision
    
    * fix formatting
    
    * fix formatting
    
    * bump rust version
    
    * fix clippy warning
    
    * fix formatting
    
    * fix cargo-fmt
    
    * fix cargo-fmt
    
    * print rustfmt version
    
    * add rust toolchain file
    
    * fix static analysis
    
    * fix formatting

--- src/protocol/sync_group/request.rs
+++ src/protocol/sync_group/request.rs
@@ -112,13 +112,12 @@ impl Assignment<'_> {
 
 impl<'a> PartitionAssignment<'a> {
     pub fn new(topic_name: &'a str, partitions: Vec<i32>) -> Self {
-        PartitionAssignment {
+        Self {
             topic_name,
             partitions,
         }
     }
 }
-
 
 impl<'a> SyncGroupRequest<'a> {
     pub fn new(
