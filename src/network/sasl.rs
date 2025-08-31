36c9d92e1c9ece1063457de105872c4e12d85f8d
Daniel Honig <daniel@boomer.digital>
2025-08-31 11:06:02 -0400

    Fix sasl auth (#131)
    * retry with suggested mechanisms
    
    * better error handling and logging
    
    * make fmt
    
    * fix clippy
    
    * fix fmt
    
    * bump rust version
    
    * clippy fix
    
    ---------
    
    Co-authored-by: alexey.nrdhm <alexey.nrdhm@gmail.com>

--- src/network/sasl.rs
+++ src/network/sasl.rs
@@ -10,7 +10,7 @@ use crate::{
 };
 use bytes::Bytes;
 use rsasl::prelude::*;
-use std::io::Cursor;
+use std::{collections::HashSet, future::Future, io::Cursor};
 
 /// SASL Credentials
 #[derive(Clone, Debug)]
@@ -62,63 +62,198 @@ pub async fn sasl_authentication(
     SaslAuthenticationResponse::try_from(authentication_response.freeze())
 }
 
-pub async fn do_sasl(
+pub async fn start_sasl_session(
     broker_conn: impl BrokerConnection + Clone,
     correlation_id: i32,
     client_id: &str,
     config: SaslConfig,
-) -> Result<()> {
-    let mechanism = String::from("SCRAM-SHA-256");
-    let handshake_response =
-        sasl_handshake(broker_conn.clone(), correlation_id, client_id, mechanism).await?;
-    if handshake_response.error_code != KafkaCode::None {
-        return Err(Error::KafkaError(handshake_response.error_code));
+    mechanism: String,
+) -> Result<(Option<Session>, Vec<String>)> {
+    let config = SASLConfig::with_credentials(None, config.username, config.password).unwrap();
+    let mut maybe_session = None;
+    let mut maybe_next = vec![];
+
+    let handshake_response = sasl_handshake(
+        broker_conn.clone(),
+        correlation_id,
+        client_id,
+        mechanism.clone(),
+    )
+    .await?;
+
+    let suggested = parse_mechanisms(&handshake_response)?;
+
+    match handshake_response.error_code {
+        KafkaCode::None => {
+            tracing::trace!("good handshake, starting a session");
+
+            let sasl = rsasl::prelude::SASLClient::new(config.clone());
+            maybe_session = Some(sasl.start_suggested(&suggested).map_err(|e| {
+                tracing::error!("{:?}", e);
+                Error::InvalidSaslMechanism
+            })?);
+        }
+        KafkaCode::UnsupportedSaslMechanism => {
+            tracing::debug!(
+                "bad handshake: {:?} not supported; will try others",
+                mechanism
+            );
+            tracing::debug!("supported mechanisms {:?}", suggested);
+            assert!(!suggested.is_empty());
+
+            maybe_next.extend(suggested.iter().map(|x| String::from(x.as_str())));
+        }
+        _ => {
+            tracing::error!("handshake failed with: {:?}", handshake_response);
+            return Err(Error::KafkaError(handshake_response.error_code));
+        }
     }
 
-    let config = SASLConfig::with_credentials(None, config.username, config.password).unwrap();
-    let sasl = rsasl::prelude::SASLClient::new(config);
-    let mechanisms = handshake_response
+    Ok((maybe_session, maybe_next))
+}
+
+fn parse_mechanisms(handshake_response: &SaslHandshakeResponse) -> Result<Vec<&Mechname>> {
+    handshake_response
         .mechanisms
         .iter()
         .map(|mech| {
             tracing::debug!("{:?}", mech);
-            Mechname::parse(mech).map_err(|e| {
+            Mechname::parse(mech.as_ref()).map_err(|e| {
                 tracing::error!("{:?}", e);
                 Error::InvalidSaslMechanism
             })
         })
-        .collect::<Result<Vec<&Mechname>>>()?;
-    tracing::debug!("mechanisms {:?}", mechanisms);
+        .collect::<Result<Vec<&Mechname>>>()
+}
 
-    let mut session = sasl.start_suggested(&mechanisms).unwrap();
-    let selected_mechanism = session.get_mechname();
-    tracing::debug!("Using {:?} for our SASL Mechanism", selected_mechanism);
+//
+// We need the factory because kafka drops our connection
+// if the given mechanism is unsupported.
+// In this case we retry with another mechanism, suggested by kafka.
+// rsasl supports this kind of workflow.
+//
+pub async fn do_sasl_v2<T, FT, FFT>(
+    conn_factory: FFT,
+    correlation_id: i32,
+    client_id: &str,
+    config: SaslConfig,
+) -> Result<T>
+where
+    T: BrokerConnection + Clone,
+    FT: Future<Output = Result<T>>,
+    FFT: Fn() -> FT,
+{
+    let mut tried = HashSet::new();
 
-    let mut data: Option<Vec<u8>> = None;
+    let mut maybe_session;
+    // Start with the given mechanism.
+    // Server will respond with a list of supported ones.
+    let mut next_mech = Some(String::from("SCRAM-SHA-256"));
 
-    // stepping the authentication exchange to completion
-    while {
-        let mut out = Cursor::new(Vec::new());
-        // each call to step writes the generated auth data into the provided writer.
-        // Normally this data would then have to be sent to the other party, but this goes
-        // beyond the scope of this example
-        let state = session
-            .step(data.as_deref(), &mut out)
-            .expect("step errored!");
+    let mut broker_conn;
 
-        data = Some(out.into_inner());
+    loop {
+        tried.insert(next_mech.clone().unwrap());
+        broker_conn = conn_factory().await?;
 
-        // returns `true` if step needs to be called again with another batch of data
-        state.is_running()
-    } {
-        let authentication_response = sasl_authentication(
+        let suggested;
+        (maybe_session, suggested) = start_sasl_session(
             broker_conn.clone(),
             correlation_id,
             client_id,
-            Bytes::from(data.unwrap()),
+            config.clone(),
+            next_mech.clone().unwrap(),
         )
         .await?;
-        data = Some(authentication_response.auth_bytes.to_vec());
+
+        next_mech = HashSet::from_iter(suggested)
+            .difference(&tried)
+            .map(|x| x.to_owned())
+            .next();
+
+        if maybe_session.is_some() || next_mech.is_none() {
+            break;
+        }
+        tracing::debug!("will retry with mechanism {:?}", next_mech);
+    }
+
+    if maybe_session.is_none() {
+        tracing::error!("failed to start a sasl session");
+        return Err(Error::InvalidSaslMechanism);
+    }
+
+    let session = maybe_session.unwrap();
+    tracing::info!("Using {:?} for our SASL Mechanism", session.get_mechname());
+
+    match do_sasl_chit_chat(session, &broker_conn, correlation_id, client_id).await {
+        Ok(_) => Ok(broker_conn),
+        Err(e) => {
+            tracing::error!("chit_chat failed: {:?}", e);
+            Err(e)
+        }
+    }
+}
+
+async fn do_sasl_chit_chat<T>(
+    mut session: Session,
+    broker_conn: &T,
+    correlation_id: i32,
+    client_id: &str,
+) -> Result<()>
+where
+    T: BrokerConnection + Clone,
+{
+    let mut data_in: Option<Vec<u8>> = None;
+
+    tracing::trace!("start sasl chit-chat");
+
+    // stepping the authentication exchange to completion
+    loop {
+        let mut out = Cursor::new(Vec::new());
+
+        // each call to step writes the generated auth data into the provided writer.
+        let state = session
+            .step(data_in.as_deref(), &mut out)
+            .expect("step errored!");
+
+        let data_out = out.into_inner();
+
+        tracing::trace!("outgoing: {:?}", data_out);
+
+        let response = sasl_authentication(
+            broker_conn.clone(),
+            correlation_id,
+            client_id,
+            Bytes::from(data_out),
+        )
+        .await?;
+
+        tracing::trace!("incoming: {:?}", response);
+
+        match response.error_code {
+            KafkaCode::None => {
+                let auth_bytes = response.auth_bytes.to_vec();
+
+                data_in = if !auth_bytes.is_empty() {
+                    Some(auth_bytes)
+                } else {
+                    None
+                };
+            }
+            KafkaCode::SaslAuthenticationFailed => {
+                let msg = response
+                    .error_message
+                    .map(|x| String::from_utf8_lossy(&x).into_owned())
+                    .unwrap_or("".to_owned());
+                tracing::info!("auth failed: {:?}: {:?}", response.error_code, msg);
+                return Err(Error::SaslAuthFailed(msg));
+            }
+            _ => return Err(Error::KafkaError(response.error_code)),
+        }
+
+        if data_in.is_none() && state.is_finished() {
+            break;
+        }
     }
 
     Ok(())
